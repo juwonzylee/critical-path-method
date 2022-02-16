85cbd2d472fa773850ae12a31ba429eda817673f
Juwon Lee <59985094+juwonzylee@users.noreply.github.com>
2022-02-16 17:29:25 +0800

    added structure

---
+++ CriticalPath.py
@@ -0,0 +1,14 @@
+class CriticalPath:
+    def __init__(self, NodesList):
+        """
+        Initializes the graph by taking in
+        a list of activity nodes
+        """
+        self.nodes = NodesList
+
+    def findCriticalPath():
+        """
+        Calculates the critical path using the activities'
+        early and late start times
+        """
+        pass
