07e394484bff0e1597ff899c3a480e0cc2f673f5
Juwon Lee <juwon.lee@u.yale-nus.edu.sg>
2022-02-18 17:43:54 +0800

    added visualization for qn8.4.10

--- README.md
+++ README.md
@@ -3,10 +3,11 @@
 A critical path in project management is the longest sequence of activities that must be finished on time in order for the entire project to be complete. This program sets the activities on edges and calculates the early, late start time, and free float of the activities 
 in addition to the critical path of the project. 
 
-The code is structured in the [cpm] module, where the nodes and edges objects are defined in [Activity.py](./cpm/Activity.py), and the project in [CriticalPath.py](./cpm/CriticalPath.py).
+The code is structured in the [cpm](./cpm) module, where the nodes and edges objects are defined in [Activity.py](./cpm/Activity.py), and the project in [CriticalPath.py](./cpm/CriticalPath.py).
 
 The tests written in [test_Activity.py](./cpm/test_Activity.py) are based on Question 8.4.4 from Chapter 8 of *Operations Research*.
 The tests written in [test_CriticalPath.py](./cpm/test_CriticalPath.py) are based on Question 8.4.10 from the same textbook.
+The network graph for Question 8.4.10 is visualized in the same test file, [test_CriticalPath.py](./cpm/test_CriticalPath.py).
 
 ## Requirements
 To install the requirements please run:
--- cpm/test_CriticalPath.py
+++ cpm/test_CriticalPath.py
@@ -3,6 +3,8 @@ from Activity import (
     ActivityEdge,
     ProjectNode
 )
+import matplotlib.pyplot as plt
+import networkx as nx
 
 # Initializing the activities
 A = ActivityEdge(3, 'A', [])
@@ -64,5 +66,63 @@ _ = J.findFreeFloat((p6, p8))
 def test_findCriticalPath():
     proj = CriticalPath([A, B, C, D, E, F, G, H, I1, J],
                         [p1, p2, p3, p4, p5, p6, p7, p8])
-
+    # assert proj.plotGraph()
     assert proj.findCriticalPath() == ['A', 'C', 'G']
+
+
+def test_visualize():
+    """
+    Visualizes the network problem used in the test file
+    """
+    # set up the tasks:
+    tasks = [("A", {"Duration": 3}),
+             ("B", {"Duration": 2}),
+             ("C", {"Duration": 6}),
+             ("D", {"Duration": 2}),
+             ("E", {"Duration": 3}),
+             ("F", {"Duration": 3}),
+             ("G", {"Duration": 5}),
+             ("H", {"Duration": 1}),
+             ("I", {"Duration": 1.5}),
+             ("J", {"Duration": 3})]
+
+    # set up the dependencies along all paths:
+    dependencies = [("A", "B"),
+                    ("A", "C"),
+                    ("C", "D"),
+                    ("A", "E"),
+                    ("B", "F"),
+                    ("C", "G"),
+                    ("C", "H"),
+                    ("F", "I"),
+                    ("H", "I"),
+                    ("I", "J")]
+
+    # initialize (directed) graph
+    G_test = nx.DiGraph()
+
+    # add tasks and dependencies (edges)
+    G_test.add_nodes_from(tasks)
+    G_test.add_edges_from(dependencies)
+
+    # set up position nodes
+    pos_nodes = {"A": (1, 1),
+                 "B": (2, 1),
+                 "C": (2, 3),
+                 "D": (3, 5),
+                 "E": (2, 0),
+                 "F": (3, 1),
+                 "G": (3, 3),
+                 "H": (3, 2),
+                 "I": (4, 1),
+                 "J": (5, 1)}
+
+    # drawing the project diagram
+    nx.draw(G_test, with_labels=True, pos=pos_nodes,
+            node_color='lightblue', arrowsize=20)
+    pos_attrs = {node: (coord[0], coord[1] + 0.2)
+                 for node, coord in pos_nodes.items()}
+    attrs = nx.get_node_attributes(G_test, 'Duration')
+    nx.draw_networkx_labels(G_test, pos=pos_attrs, labels=attrs)
+    plt.margins(0.1)
+    plt.show()
